45bb546a8a5900fa720780770d3083ccdc6c9f34
rajbudigam <163419369+rajbudigam@users.noreply.github.com>
2025-07-07 15:46:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='carbonswipe',
-    version='1.0.0',
+    name="carbonswipe",
+    version="1.0.0",
     packages=find_packages(),
-    install_requires=[],
-    author='Raj Budigam',
-    description='Track energy, CO2, and water use from local LLM inference.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/rajbudigam/CarbonSwipe',
-    classifiers=[
-        'Programming Language :: Python :: 3',
+    install_requires=[
+        # e.g. "codecarbon>=2.1.4",
     ],
+    author="Jasraj Budigam",
+    description="Measure energy, CO2 & water use of local LLM inference",
+    url="https://github.com/rajbudigam/CarbonSwipe",
+    python_requires=">=3.7",
 )
